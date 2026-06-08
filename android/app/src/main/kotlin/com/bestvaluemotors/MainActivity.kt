f98698bc04ebcb872709537d8b6de78c710e8e8b
Pooja <poojaodd2001@gmail.com>
2026-06-08 17:22:23 +0530

    First Commit

---
+++ android/app/src/main/kotlin/com/bestvaluemotors/MainActivity.kt
@@ -0,0 +1,4 @@
+package com.bestvaluemotors
+
+import io.flutter.embedding.android.FlutterActivity
+class MainActivity : FlutterActivity()
---
+++ lib/utils/appColors.dart
@@ -0,0 +1,12 @@
+import 'dart:ui';
+
+class AppColors {
+  static const Color orangeColor = Color(0xFFFF6A00);
+  static const Color bgColor = Color(0xFF050505);
+  static const Color cardBgColor = Color(0xFF0D0D0D);
+  static const Color borderColor = Color(0xFF2A2A2A);
+  static const Color whiteColor = Color(0xFFFFFFFF);
+  static const Color greyColor = Color(0xFFBDBDBD);
+  static const Color dividerColor = Color(0xFF3A3A3A);
+  static const Color green = Color(0xFF00E676);
+}
